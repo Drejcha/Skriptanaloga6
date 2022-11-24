6783049f59a9d87e27f1d4c3b2b0222d635b288b
Drejcha <85193155+Drejcha@users.noreply.github.com>
2022-11-24 19:54:19 +0100

    Add files via upload

---
+++ skriptadrejcgruden.sh
@@ -0,0 +1,50 @@
+
+mkdir Windows
+cd Windows
+mkdir Desktop Dokumenti Prenosi Slike Videi
+cd ..
+for stevec in 1 2 3 4 5
+do
+mkdir mapa$stevec
+done
+
+echo -n "Enter name of text file: "; read FILENAME
+while read USER; do
+   echo "$USER"
+  sudo  useradd -m "${USER}" -d /users/"${USER}"
+   sudo  usermod -aG sudo -m "${USER}"
+done < "${FILENAME}"
+
+sudo apt update && upgrade
+
+
+sudo apt-get install git
+sudo wget http://nginx.org/download/nginx-1.21.1.tar.gz
+tar -xf nginx-1.21.1.tar.gz
+cd nginx-1.21.1
+./configure
+sleep 5
+cd ..
+sudo apt install net-tools -y
+
+sudo apt-get update
+sudo apt-get install \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/>
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+
+sudo docker run hello-world
+
+
+
+
+
+
